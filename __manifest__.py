eb3f133b1ba7e444745f92854500a9a73631c749
SerBarcla <97915214+SerBarcla@users.noreply.github.com>
2023-05-31 00:15:19 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,7 @@
     'name': 'My Odoo App',
     'version': '1.0',
     'summary': 'An app to input date and description',
+    'icon': 'icon.png',
     'description': 'A custom Odoo app that takes a date and description as input',
     'author': 'Your Name',
     'category': 'Custom',
