7252b477a587bd1e9feb941deacc1b983c76216e
SerBarcla <97915214+SerBarcla@users.noreply.github.com>
2023-05-31 00:08:03 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'My Odoo App',
+    'version': '1.0',
+    'summary': 'An app to input date and description',
+    'description': 'A custom Odoo app that takes a date and description as input',
+    'author': 'Your Name',
+    'category': 'Custom',
+    'depends': ['base'],
+    'data': [
+        'views/app_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
